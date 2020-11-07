4bb7864ab624f4638367435b90631a644ffc52fe
Ning Ding <n2dingn2ding@gmail.com>
2020-11-06 17:21:35 -0800

    chang class name for status; set status with db

---
+++ scripts/station.js
@@ -0,0 +1,26 @@
+const seating = ["Empty", "Seating Only", "Full"]
+const timing = ["On time", "Late", "Very late"]
+const mask = ["Complete", "Parial", "Few"]
+const colors = ["-green", "-yellow", "-red"]
+
+function readReport() {
+    db.collection("stations").doc("CentralBlvdE").get().then(function(doc) {
+        if (doc.data().name == "Central Blvd E") {
+            var report = doc.data().reports[0]
+        }
+
+        let seatingStatus = document.getElementsByClassName("-seating")[0]
+        seatingStatus.innerText = seating[report.seating]
+        seatingStatus.classList.add(colors[report.seating])
+
+        let timingStatus = document.getElementsByClassName("-timing")[0]
+        timingStatus.innerText = timing[report.timing]
+        timingStatus.classList.add(colors[report.timing])
+
+        let maskStatus = document.getElementsByClassName("-mask")[0]
+        maskStatus.innerText = mask[report.masks]
+        maskStatus.classList.add(colors[report.masks])
+        console.log(report)
+    })
+}
+readReport();
