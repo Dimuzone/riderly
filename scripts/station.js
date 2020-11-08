0ad89190f22f8403281ec9ce4954e28f99ddf499
dimuhtu kumarapeli <dayankumarapeli92@gmail.com>
2020-11-07 16:13:01 -0800

    Finish the saving favourite stations

--- scripts/station.js
+++ scripts/station.js
@@ -22,3 +22,54 @@ db.collection("reports")
         maskStatus.innerText = mask[report.masks]
         maskStatus.classList.add(colors[report.masks])
     })
+
+const station = 52500
+const star = document.getElementById("star")
+
+
+star.onclick = function onClick() {
+    if (star.innerText === "star_border") {
+        saveStation(station)
+        star.innerText = "star"
+
+    } else if (star.innerText === "star") {
+        removeStation(station)
+        star.innerText = "star_border"
+    }
+
+}
+
+
+
+function saveStation(station) {
+    firebase.auth().onAuthStateChanged(user => {
+        let id = user.uid
+        db.collection("users").doc(id).get().then(user => {
+
+            let saves = user.data().saves.slice()
+
+            saves.push(station)
+
+            db.collection("users").doc(id).update( {saves} )
+
+        })
+    
+    })
+}
+
+function removeStation(station) {
+    firebase.auth().onAuthStateChanged(user => {
+        let id = user.uid
+        db.collection("users").doc(id).get().then(user => {
+
+            let saves = user.data().saves.slice()
+
+            if (saves.includes(station)) {
+                saves.splice(saves.indexOf(station), 1)
+            }
+
+            db.collection("users").doc(id).update( {saves} )
+
+        })
+    })
+}
