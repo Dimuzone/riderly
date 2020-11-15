3137c56152ce06c673701293044c47668bab8dc5
Dimuzone <71089439+Dimuzone@users.noreply.github.com>
2020-11-15 08:37:10 +0000

    Deploying to gh-pages from  @ fb4e23ee68ed98779c364b34e12c46805cf8d7dc 🚀

--- scripts/search.js
+++ scripts/search.js
@@ -28,11 +28,16 @@ async function main() {
 
 
 function renderRoute(route) {
-    return div({ class: "option" }, [
-        div({ class: "option-data" }, [
-            p({ class: "option-text" }, route.id),
-            div({ class: "option-subtext" }, route.data.name),
-        ]),
-        span({ class: "option-icon material-icons" }, "chevron_right")
+    function onclick() {
+        sessionStorage.setItem("route", route.id)
+        sessionStorage.setItem("stationId", route.data.path)
+        location.href = "route.html"
+      }
+    return div({ class: "option", onclick: onclick }, [
+      div({ class: "option-data"}, [
+      p({ class: "option-text" }, route.id),
+      div({ class: "option-subtext" }, route.data.name),
+      ]),
+      span({ class: "option-icon material-icons"}, "chevron_right")
     ])
 }
