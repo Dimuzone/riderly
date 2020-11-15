da2d740e2fa6ca9e555c2268bdff18069371f89b
zzzbnbzzz <65437810+zzzbnbzzz@users.noreply.github.com>
2020-11-15 05:44:14 +0000

    Deploying to gh-pages from  @ 743d5547b1f85e28200a03c00552fa07b4bdeca4 🚀

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,22 @@ firebase.auth().onAuthStateChanged(user => {
     db.collection("users").doc(user.uid).get().then(users => {
 
         console.log(users.data().saves)
-        
+
     })
 
+    let loginstatus = document.getElementsByClassName("login-text")[0]
+    loginstatus.innerText = "Logout"
+
 })
+
+let button = document.getElementsByClassName("login")[0]
+
+var signin = firebase.auth().currentUser;
+
+button.onclick = _ => {
+    if (signin) {
+        firebase.auth().signOut().then(_ => location.href = "index.html")
+    } else {
+        location.href = "login.html"
+    }
+}
