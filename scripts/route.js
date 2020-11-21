f32664a850ab7c5963f37454f34d0b0da08eec08
Dimuzone <71089439+Dimuzone@users.noreply.github.com>
2020-11-21 09:57:58 +0000

    Deploying to gh-pages from  @ e4aa1f8731cacfeb4dbcdef84cfeb47f3b3889e0 🚀

--- scripts/route.js
+++ scripts/route.js
@@ -1,6 +1,7 @@
 //const stations = [{"id":58143,"name":"Kootenay Loop @ Bay 8","lat":49.281026,"lon":-123.024952,"zone":"BUS ZN"},{"id":56232,"name":"Eastbound Marine Dr @ Magdalen Cres","lat":49.026278,"lon":-122.826124,"zone":"BUS ZN"},{"id":59842,"name":"Northbound Main St @ E 46 Ave","lat":49.228719,"lon":-123.101386,"zone":"BUS ZN"},{"id":61492,"name":"Southbound 156 St @ 110 Ave","lat":49.202113,"lon":-122.789933,"zone":"BUS ZN"},{"id":57757,"name":"Eastbound 123 Ave @ 206 St","lat":49.226234,"lon":-122.649804,"zone":"BUS ZN"},{"id":60791,"name":"Westbound 64 Ave @ 177B St","lat":49.118858,"lon":-122.731356,"zone":"BUS ZN"},{"id":57814,"name":"Westbound 123 Ave @ 216 St","lat":49.226474,"lon":-122.623375,"zone":"BUS ZN"},{"id":57933,"name":"Westbound Tamarack Lane @ Lougheed Hwy","lat":49.201243,"lon":-122.577748,"zone":"BUS ZN"},{"id":57357,"name":"Westbound 12 Ave @ Jackson Way","lat":49.024722,"lon":-123.063407,"zone":"BUS ZN"},{"id":59720,"name":"Westbound Holly Park Dr @ 62 St","lat":49.087941,"lon":-123.050768,"zone":"BUS ZN"},{"id":51916,"name":"Westbound Chancellor Blvd @ Hamber Rd","lat":49.273342,"lon":-123.239004,"zone":"BUS ZN"},{"id":54563,"name":"Southbound Marine Dr @ Eagleridge Dr","lat":49.361175,"lon":-123.277897,"zone":"BUS ZN"},{"id":51435,"name":"Northbound Canada Way @ 14 Ave","lat":49.220067,"lon":-122.935607,"zone":"BUS ZN"},{"id":51808,"name":"Northbound Elliott St @ E 54 Ave","lat":49.220497,"lon":-123.0548,"zone":"BUS ZN"},{"id":50199,"name":"Eastbound W 41 Ave @ Granville St","lat":49.23422,"lon":-123.138484,"zone":"BUS ZN"},{"id":60075,"name":"Northbound Pipeline Rd @ Windsor Gate","lat":49.280631,"lon":-122.787013,"zone":"BUS ZN"},{"id":51702,"name":"Southbound Rupert St @ Grant St","lat":49.27109,"lon":-123.033737,"zone":"BUS ZN"},{"id":58988,"name":"Westbound Kingsway @ Nelson Ave","lat":49.227163,"lon":-122.995118,"zone":"BUS ZN"},{"id":60500,"name":"Eastbound University Dr @ Mattson Centre","lat":49.141737,"lon":-122.599236,"zone":"BUS ZN"},{"id":54928,"name":"Southbound 112 St @ 90 Ave","lat":49.167281,"lon":-122.912436,"zone":"BUS ZN"},{"id":60424,"name":"Eastbound Blundell Rd @ York Rd","lat":49.155303,"lon":-123.043518,"zone":"BUS ZN"},{"id":51205,"name":"Northbound Victoria Dr @ Brigadoon Ave","lat":49.21691,"lon":-123.065874,"zone":"BUS ZN"},{"id":50391,"name":"Eastbound W 4th Ave @ Trutch St","lat":49.268381,"lon":-123.17537,"zone":"BUS ZN"},{"id":58787,"name":"Northbound Coast Meridian Rd @ Patricia Ave","lat":49.276772,"lon":-122.756005,"zone":"BUS ZN"},{"id":55790,"name":"Eastbound 108 Ave @ King George Blvd","lat":49.19876,"lon":-122.844888,"zone":"BUS ZN"},{"id":59076,"name":"Northbound Railway Ave @ 11100 Block","lat":49.132141,"lon":-123.169697,"zone":"BUS ZN"},{"id":61961,"name":"Northbound 156 St @ Hwy One Offramp","lat":49.194903,"lon":-122.789679,"zone":"BUS ZN"},{"id":57240,"name":"Eastbound 88 Ave @ 212 St","lat":49.162127,"lon":-122.632424,"zone":"BUS ZN"},{"id":51838,"name":"Eastbound SE Marine Dr @ Fieldstone Ave","lat":49.208333,"lon":-123.035248,"zone":"BUS ZN"},{"id":58806,"name":"Westbound Oxford Connector @ Oxford St","lat":49.263833,"lon":-122.768302,"zone":"BUS ZN"},{"id":58625,"name":"Northbound Lonsdale Ave @ E 19 St","lat":49.326627,"lon":-123.072103,"zone":"BUS ZN"},{"id":55571,"name":"Northbound 128 St @ 69A Ave","lat":49.129721,"lon":-122.867682,"zone":"BUS ZN"},{"id":54796,"name":"Westbound 72 Ave @ 118 St","lat":49.134087,"lon":-122.896338,"zone":"BUS ZN"},{"id":53015,"name":"Northbound Mariner Way @ Lighthouse Court","lat":49.263884,"lon":-122.816831,"zone":"BUS ZN"},{"id":58947,"name":"Westbound E 6th Ave @ Glenbrook Dr","lat":49.22081,"lon":-122.90625,"zone":"BUS ZN"},{"id":54562,"name":"Southbound Marine Dr @ Orchill Rd","lat":49.363028,"lon":-123.277509,"zone":"BUS ZN"},{"id":53771,"name":"Northbound Western Dr @ Eastern Dr","lat":49.240697,"lon":-122.783681,"zone":"BUS ZN"},{"id":53338,"name":"Northbound Schoolhouse St @ Winslow Ave","lat":49.25422,"lon":-122.852146,"zone":"BUS ZN"},{"id":53211,"name":"Southbound North Rd @ Foster Ave","lat":49.255871,"lon":-122.893156,"zone":"BUS ZN"},{"id":57041,"name":"Northbound 200 St @ 7600 Block","lat":49.141434,"lon":-122.668059,"zone":"BUS ZN"},{"id":50400,"name":"Eastbound W 4th Ave @ Burrard St","lat":49.26793,"lon":-123.145082,"zone":"BUS ZN"},{"id":53413,"name":"Southbound Hickey Dr @ Austin Ave","lat":49.248565,"lon":-122.824482,"zone":"BUS ZN"},{"id":55651,"name":"Northbound 132 St @ 72 Ave","lat":49.134109,"lon":-122.85658,"zone":"BUS ZN"},{"id":60054,"name":"Eastbound David Ave @ Riley St","lat":49.294609,"lon":-122.743629,"zone":"BUS ZN"},{"id":54565,"name":"Eastbound Marine Dr @ 5800 Block","lat":49.35715,"lon":-123.27306,"zone":"BUS ZN"},{"id":55101,"name":"Westbound 114 Ave @ 124 St","lat":49.209892,"lon":-122.877437,"zone":"BUS ZN"},{"id":54942,"name":"Northbound 122 St @ 7300 Block","lat":49.135461,"lon":-122.884518,"zone":"BUS ZN"},{"id":56249,"name":"Northbound 160 St @ Cliff Ave","lat":49.022218,"lon":-122.779154,"zone":"BUS ZN"},{"id":50451,"name":"Northbound Oak St @ W 38 Ave","lat":49.237021,"lon":-123.12785,"zone":"BUS ZN"},{"id":54950,"name":"Westbound River Rd @ Russell Dr","lat":49.16712,"lon":-122.917038,"zone":"BUS ZN"},{"id":61533,"name":"Northbound E Columbia St @ Cedar St","lat":49.230458,"lon":-122.892653,"zone":"BUS ZN"},{"id":99920,"name":"Gilmore Station","lat":49.264974,"lon":-123.013554,"zone":"ZN 2"},{"id":51650,"name":"Southbound Frontenac St @ Hurst Ave","lat":49.220252,"lon":-123.026594,"zone":"BUS ZN"},{"id":56874,"name":"Southbound Garden City Rd @ Saunders Rd","lat":49.14325,"lon":-123.125279,"zone":"BUS ZN"},{"id":50273,"name":"Blanca Loop @ Bay 1","lat":49.265243,"lon":-123.21585,"zone":"BUS ZN"},{"id":56905,"name":"Eastbound Westminster Hwy @ McLean Ave","lat":49.174718,"lon":-122.969833,"zone":"BUS ZN"},{"id":53491,"name":"Westbound Austin Ave @ Whiting Way","lat":49.249197,"lon":-122.890392,"zone":"BUS ZN"},{"id":53911,"name":"Southbound Lansdowne Dr @ Charter Hill Dr","lat":49.285884,"lon":-122.812691,"zone":"BUS ZN"},{"id":56932,"name":"Westbound Cambie Rd @ Shell Rd","lat":49.18467,"lon":-123.103428,"zone":"BUS ZN"},{"id":60240,"name":"Lenora Rd @ Melmore Rd (Flag)","lat":49.383944,"lon":-123.336686,"zone":"BUS ZN"},{"id":53422,"name":"Eastbound Cape Horn Ave @ Colony Farm Rd","lat":49.241138,"lon":-122.813498,"zone":"BUS ZN"},{"id":57284,"name":"Southbound 222 St @ Fraser Hwy","lat":49.091094,"lon":-122.608245,"zone":"BUS ZN"},{"id":53192,"name":"Northbound College Park Way @ Oxford Dr","lat":49.283349,"lon":-122.871204,"zone":"BUS ZN"},{"id":56401,"name":"Southbound 128 St @ 76 Ave","lat":49.140618,"lon":-122.868045,"zone":"BUS ZN"},{"id":58466,"name":"Eastbound 25 Ave @ 126 St","lat":49.048065,"lon":-122.872644,"zone":"BUS ZN"},{"id":58703,"name":"Southbound Oxford St @ Forestgate Place","lat":49.291226,"lon":-122.763205,"zone":"BUS ZN"},{"id":50980,"name":"Lonsdale Quay @ Bay 5","lat":49.311218,"lon":-123.082407,"zone":"BUS ZN"},{"id":51407,"name":"Southbound Canada Way @ Morley Dr","lat":49.230217,"lon":-122.948543,"zone":"BUS ZN"},{"id":55466,"name":"Southbound King George Blvd @ 58 Ave","lat":49.107904,"lon":-122.83566,"zone":"BUS ZN"},{"id":58765,"name":"Eastbound 88 Ave @ 204 St","lat":49.162711,"lon":-122.656336,"zone":"BUS ZN"},{"id":52326,"name":"Eastbound Cumberland St @ Glenbrook Dr","lat":49.220507,"lon":-122.899746,"zone":"BUS ZN"},{"id":54547,"name":"Northbound Capilano Rd @ Ridgewood Dr","lat":49.339994,"lon":-123.114328,"zone":"BUS ZN"},{"id":55997,"name":"Westbound 60 Ave @ 148 St","lat":49.111845,"lon":-122.812964,"zone":"BUS ZN"},{"id":51705,"name":"Southbound Rupert St @ E 5th Ave","lat":49.265956,"lon":-123.03388,"zone":"BUS ZN"},{"id":53302,"name":"Eastbound Lougheed Hwy @ Holdom Ave","lat":49.264649,"lon":-122.980395,"zone":"BUS ZN"},{"id":52835,"name":"Westbound Lougheed Hwy @ Sperling Ave","lat":49.25941,"lon":-122.962966,"zone":"BUS ZN"},{"id":51628,"name":"Northbound Boundary Rd @ Hurst Ave","lat":49.220212,"lon":-123.023547,"zone":"BUS ZN"},{"id":59678,"name":"Northbound Patterson Ave @ Rumble St","lat":49.215171,"lon":-123.0123,"zone":"BUS ZN"},{"id":59650,"name":"Southbound Laity St @ 12200 Block","lat":49.225729,"lon":-122.631874,"zone":"BUS ZN"},{"id":53585,"name":"Westbound 6th Ave @ 12 St","lat":49.207443,"lon":-122.931048,"zone":"BUS ZN"},{"id":51660,"name":"Westbound Rosemont Dr @ Butler St","lat":49.216141,"lon":-123.035466,"zone":"BUS ZN"},{"id":55126,"name":"Southbound 150 St @ 107A Ave","lat":49.197569,"lon":-122.806329,"zone":"BUS ZN"},{"id":50072,"name":"Eastbound Cornwall Ave @ Cypress St","lat":49.272368,"lon":-123.14734,"zone":"BUS ZN"},{"id":58851,"name":"Northbound Paddock Dr @ Plateau Blvd","lat":49.307822,"lon":-122.78704,"zone":"BUS ZN"},{"id":50681,"name":"Southbound Nanaimo St @ E 7th Ave","lat":49.26381,"lon":-123.056749,"zone":"BUS ZN"},{"id":54006,"name":"Southbound Mountain Hwy @ Kirkstone Rd","lat":49.326875,"lon":-123.038326,"zone":"BUS ZN"},{"id":57305,"name":"Eastbound Fraser Hwy @ 268 St","lat":49.058455,"lon":-122.481117,"zone":"BUS ZN"},{"id":59541,"name":"Eastbound Canoe Pass Way @ Salish Sea Dr","lat":49.04011,"lon":-123.089288,"zone":"BUS ZN"},{"id":50926,"name":"Westbound W Broadway @ Willow St","lat":49.263433,"lon":-123.122318,"zone":"BUS ZN"},{"id":53019,"name":"Eastbound Mariner Way @ Windward Dr","lat":49.272582,"lon":-122.811201,"zone":"BUS ZN"},{"id":54708,"name":"Westbound Caulfeild Dr @ Cherbourg Dr","lat":49.346473,"lon":-123.249222,"zone":"BUS ZN"},{"id":51862,"name":"Westbound S Campus Rd @ Science Rd","lat":49.276558,"lon":-122.916031,"zone":"BUS ZN"},{"id":53754,"name":"Northbound Barnet Hwy @ Union St","lat":49.287092,"lon":-122.867643,"zone":"BUS ZN"},{"id":51963,"name":"Eastbound W 49 Ave @ Oak St","lat":49.22645,"lon":-123.12776,"zone":"BUS ZN"},{"id":53287,"name":"Northbound Turner Creek Dr @ Parkglen Place","lat":49.294453,"lon":-122.836167,"zone":"BUS ZN"},{"id":61119,"name":"Westbound E 33 Ave @ Ontario St","lat":49.240633,"lon":-123.105056,"zone":"BUS ZN"},{"id":59771,"name":"Lansdowne Station @ Bay 4","lat":49.172869,"lon":-123.136815,"zone":"BUS ZN"},{"id":52890,"name":"Northbound Duthie Ave @ Greystone Dr","lat":49.273013,"lon":-122.953411,"zone":"BUS ZN"},{"id":53761,"name":"Southbound Inlet Dr @ Ridge Dr","lat":49.282881,"lon":-122.9565,"zone":"BUS ZN"},{"id":61862,"name":"Southbound Blakely Rd @ Hammond Rd","lat":49.216944,"lon":-122.683861,"zone":"BUS ZN"}]
 
-const stationWrap = document.getElementById("routes")
+const search = document.getElementById("search-bar")
+const stationWrap = document.getElementById("stations")
 let route = sessionStorage.getItem("route")
 let path = sessionStorage.getItem("stationId").split(",").map(Number)
 //let stationId = StationArray.split(",").map(Number)
@@ -9,21 +10,52 @@ console.log(path)
 main()
 
 async function main() {
-  let stationData = {}
+  let stationData = []
 
   for (let i = 0; i < path.length; i += 10) {
 
     let chunk = path.slice(i, i + 10)
     console.log(chunk.length)
     let col = await db.collection("stations").where("id", "in", chunk).get()
-    col.forEach(doc => stationData[doc.id] = doc.data())
+    col.forEach(doc => stationData.push(doc.data()))
 
   }
 
-    console.log(stationData)
+  render(stationData)
 
-    patch(stationWrap,
-       div({class: "routes" }, path.map(id => renderStation(stationData[id]))))
+  search.oninput = _ => {
+    let stations = stationData.filter(station =>
+      station.id.toString().includes(search.value)
+      || station.name.toLowerCase().includes(search.value.toLowerCase())
+    )
+    render(stations)
+  }
+
+}
+
+function render(stations) {
+  patch(stationWrap, div({ id: "stations" }, stations.map(renderStation)))
+}
+document.getElementById("route").innerText = "Route " + route
+
+document.getElementById("swap").onclick = function () {
+  char = route[route.length - 1]
+  
+  let newRoute =  route.slice(0, -1) + swapChar(char)
+
+  route = newRoute
+
+  document.getElementById("route").innerText = "Route " + route
+
+}
+
+function swapChar(char) {
+  switch(char) {
+    case "W" : return "E"
+    case "E" : return "W"
+    case "S" : return "N"
+    case "N" : return "S"
+  } 
 }
 
 
