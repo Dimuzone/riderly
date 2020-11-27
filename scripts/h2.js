2b49a4a508040ad9e9751ccdb3ee2744786bc539
semibran <semibran+gh@gmail.com>
2020-11-27 23:21:36 +0000

    Deploying to gh-pages from  @ 9f3be5b62e78ddb6814966f4000f807dc35df972 🚀

--- scripts/h2.js
+++ scripts/h2.js
@@ -8,156 +8,156 @@
 // Includes a patch function for updating DOM elements to match vnodes
 //
 (function (tags) {
-	// Create contextualized h functions
-	var EMPTY_OBJ = {}
-	for (var i = 0; i < tags.length; i++) {
-		let tag = tags[i]
-		window[tag] = (function (h, tag) {
-			return function (data, content) {
-				if (data === undefined || Array.isArray(data) || typeof data !== "object") {
-					content = data
-					data = EMPTY_OBJ
-				}
-				if (content === undefined) {
-					content = []
-				} else if (!Array.isArray(content)) {
-					content = [ content ]
-				}
-				return h(tag, data, content)
-			}
-		})(h, tag)
-	}
+  // Create contextualized h functions
+  const EMPTY_OBJ = {}
+  for (let i = 0; i < tags.length; i++) {
+    const tag = tags[i]
+    window[tag] = (function (h, tag) {
+      return function (data, content) {
+        if (data === undefined || Array.isArray(data) || typeof data !== 'object') {
+          content = data
+          data = EMPTY_OBJ
+        }
+        if (content === undefined) {
+          content = []
+        } else if (!Array.isArray(content)) {
+          content = [content]
+        }
+        return h(tag, data, content)
+      }
+    })(h, tag)
+  }
 
-	window.patch = patch
+  window.patch = patch
 
-	// h(tag, data, [ vnode | prim ]) -> vnode
-	// Creates a vnode.
-	function h(tag, data, content) {
-		return {
-			tag: tag,
-			data: data,
-			content: content
-		}
-	}
+  // h(tag, data, [ vnode | prim ]) -> vnode
+  // Creates a vnode.
+  function h (tag, data, content) {
+    return {
+      tag: tag,
+      data: data,
+      content: content
+    }
+  }
 
-	// manifest(vnode) -> Element
-	// Converts a vnode to an HTML element.
-	function manifest(node) {
-		if (node instanceof Element) return node
-		if (!node || typeof node !== "object") {
-			return document.createTextNode(node)
-		}
-		let tag = node.tag
-		let data = node.data
-		let content = node.content
-		let element = document.createElement(tag)
-		for (let name in data) {
-			let value = data[name]
-			element[name] = value
-			if (typeof value !== "function") {
-				element.setAttribute(name, value)
-			}
-		}
-		for (let i = 0; i < content.length; i++) {
-			let child = manifest(content[i])
-			element.appendChild(child)
-		}
-		return element
-	}
+  // manifest(vnode) -> Element
+  // Converts a vnode to an HTML element.
+  function manifest (node) {
+    if (node instanceof window.Element) return node
+    if (!node || typeof node !== 'object') {
+      return document.createTextNode(node)
+    }
+    const tag = node.tag
+    const data = node.data
+    const content = node.content
+    const element = document.createElement(tag)
+    for (const name in data) {
+      const value = data[name]
+      element[name] = value
+      if (typeof value !== 'function') {
+        element.setAttribute(name, value)
+      }
+    }
+    for (let i = 0; i < content.length; i++) {
+      const child = manifest(content[i])
+      element.appendChild(child)
+    }
+    return element
+  }
 
-	// patch(Element, vnode)
-	// Updates an existing DOM element to match the given vnode.
-	function patch(el, node) {
-		if (!(el instanceof Element)) {
-			throw new Error("Patch operation failed: No target element specified")
-		}
+  // patch(Element, vnode)
+  // Updates an existing DOM element to match the given vnode.
+  function patch (el, node) {
+    if (!(el instanceof window.Element)) {
+      throw new Error('Patch operation failed: No target element specified')
+    }
 
-		if (Array.isArray(node)) {
-			throw new Error("Patch operation failed: Cannot patch from an array of nodes")
-		}
+    if (Array.isArray(node)) {
+      throw new Error('Patch operation failed: Cannot patch from an array of nodes')
+    }
 
-		let tag = el.tagName
-		let data = el.attributes
-		let content = el.childNodes
+    const tag = el.tagName
+    const data = el.attributes
+    const content = el.childNodes
 
-		// just create a new element if the new tag is different
-		if (typeof tag !== typeof node.tag || tag !== node.tag.toUpperCase()) {
-			let newel = manifest(node)
-			if (el.parentNode) {
-				el.parentNode.replaceChild(newel, el)
-			}
-			return newel
-		}
+    // just create a new element if the new tag is different
+    if (typeof tag !== typeof node.tag || tag !== node.tag.toUpperCase()) {
+      const newel = manifest(node)
+      if (el.parentNode) {
+        el.parentNode.replaceChild(newel, el)
+      }
+      return newel
+    }
 
-		// remove attributes on old element
-		// if they are missing from new node
-		for (let i = 0; i < data.length; i++) {
-			let attr = data[i]
-			let attrid = attr.name
-			if (!node.data[attrid]) {
-				el.removeAttribute(attrid)
-			}
-		}
+    // remove attributes on old element
+    // if they are missing from new node
+    for (let i = 0; i < data.length; i++) {
+      const attr = data[i]
+      const attrid = attr.name
+      if (!node.data[attrid]) {
+        el.removeAttribute(attrid)
+      }
+    }
 
-		// add new node attributes to old element
-		// if they are missing from old element
-		for (let attrid in node.data) {
-			let attrval = node.data[attrid]
-			if (typeof attrval === "function") {
-				if (el[attrid] === attrval) continue
-				el[attrid] = attrval
-			} else if (el.getAttribute(attrid) !== attrval.toString()) {
-				el.setAttribute(attrid, attrval)
-			}
-		}
+    // add new node attributes to old element
+    // if they are missing from old element
+    for (const attrid in node.data) {
+      const attrval = node.data[attrid]
+      if (typeof attrval === 'function') {
+        if (el[attrid] === attrval) continue
+        el[attrid] = attrval
+      } else if (el.getAttribute(attrid) !== attrval.toString()) {
+        el.setAttribute(attrid, attrval)
+      }
+    }
 
-		// remove extra children from old element
-		// if they are missing from new element
-		// TODO: determine if there's a faster way
-		//   to find which elements were removed
-		while (content.length > node.content.length) {
-			el.removeChild(content[content.length - 1])
-		}
+    // remove extra children from old element
+    // if they are missing from new element
+    // TODO: determine if there's a faster way
+    //   to find which elements were removed
+    while (content.length > node.content.length) {
+      el.removeChild(content[content.length - 1])
+    }
 
-		// ignore whitespace
-		for (let i = 0; i < content.length; i++) {
-			let child = content[i]
-			if (child instanceof Text && !child.data.trim()) {
-				el.removeChild(child)
-			}
-		}
+    // ignore whitespace
+    for (let i = 0; i < content.length; i++) {
+      const child = content[i]
+      if (child instanceof window.Text && !child.data.trim()) {
+        el.removeChild(child)
+      }
+    }
 
-		// patch remaining children
-		for (let i = 0; i < node.content.length; i++) {
-			let child = content[i]
-			let newchild = node.content[i]
-			if (!child) {
-				// nothing to patch, add a new element
-				el.appendChild(manifest(newchild))
-			} else if (child.nodeName !== "#comment" && (child instanceof Element
-			|| typeof newchild === "object")) {
-				// general situation: patch child to reflect new child data
-				patch(child, newchild)
-			} else if (child.data !== newchild) {
-				// for textnode: just change content
-				child.data = newchild
-			}
-		}
+    // patch remaining children
+    for (let i = 0; i < node.content.length; i++) {
+      const child = content[i]
+      const newchild = node.content[i]
+      if (!child) {
+        // nothing to patch, add a new element
+        el.appendChild(manifest(newchild))
+      } else if (child.nodeName !== '#comment' && (child instanceof window.Element ||
+      typeof newchild === 'object')) {
+        // general situation: patch child to reflect new child data
+        patch(child, newchild)
+      } else if (child.data !== newchild) {
+        // for textnode: just change content
+        child.data = newchild
+      }
+    }
 
-		return el
-	}
+    return el
+  }
 })([
-	"a", "abbr", "address", "area", "article", "aside", "audio", "b", "base",
-	"bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption",
-	"cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details",
-	"dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption",
-	"figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head",
-	"header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd",
-	"label", "legend", "li", "link", "main", "map", "mark", "math", "menu",
-	"meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option",
-	"output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby",
-	"s", "samp", "script", "section", "select", "slot", "small", "source", "span",
-	"strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td",
-	"template", "textarea", "tfoot", "th", "thead", "time", "title", "tr",
-	"track", "u", "ul", "video", "wbr"
+  'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base',
+  'bdi', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption',
+  'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details',
+  'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption',
+  'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head',
+  'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd',
+  'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'math', 'menu',
+  'meta', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option',
+  'output', 'p', 'param', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby',
+  's', 'samp', 'script', 'section', 'select', 'slot', 'small', 'source', 'span',
+  'strong', 'style', 'sub', 'summary', 'sup', 'svg', 'table', 'tbody', 'td',
+  'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr',
+  'track', 'u', 'ul', 'video', 'wbr'
 ])
