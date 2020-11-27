9c0fdd11e4f949b0c9b42e1e954c3ad315acfbb6
Brandon Semilla <semibran@gmail.com>
2020-11-27 15:16:33 -0800

    db: fix standardjs warnings

--- public/scripts/db.js
+++ public/scripts/db.js
@@ -1,12 +1,12 @@
 // init firebase
-firebase.initializeApp({
-	apiKey: "AIzaSyBYV23zeovHmzX26j9H9HYyI9kBlAzZcvg",
-	authDomain: "bus-dating.firebaseapp.com",
-	databaseURL: "https://bus-dating.firebaseio.com",
-	projectId: "bus-dating",
-	storageBucket: "bus-dating.appspot.com",
-	messagingSenderId: "1012927739228",
-	appId: "1:1012927739228:web:ce582ea70ee1c0f25b7d99"
+window.firebase.initializeApp({
+  apiKey: 'AIzaSyBYV23zeovHmzX26j9H9HYyI9kBlAzZcvg',
+  authDomain: 'bus-dating.firebaseapp.com',
+  databaseURL: 'https://bus-dating.firebaseio.com',
+  projectId: 'bus-dating',
+  storageBucket: 'bus-dating.appspot.com',
+  messagingSenderId: '1012927739228',
+  appId: '1:1012927739228:web:ce582ea70ee1c0f25b7d99'
 })
 
-let db = firebase.firestore()
+window.db = window.firebase.firestore()
