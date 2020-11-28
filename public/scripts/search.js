2fcdd424a4bbcb7aeed2dbfdfecd2268a741f0b6
Brandon Semilla <semibran@gmail.com>
2020-11-27 19:32:21 -0800

    revise search page

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,39 +1,43 @@
 const { db, patch, div, p, span } = window
+const search = document.getElementById('search-bar')
+const routewrap = document.getElementById('routes')
 
-// Main function for getting the routes and calling the render function to set the page
-main()
-async function main () {
-  const col = await db.collection('routes').get()
+const state = { routes: [] }
+
+;(async function main () {
+  // Get all routes from Firestore and rerender
   const routes = []
-  col.forEach(doc => routes.push({
-    id: doc.id,
-    data: doc.data()
-  }))
-  const search = document.getElementById('search-bar')
-  const routeWrap = document.getElementById('routes')
-  search.oninput = _ => {
-    const routeSearch = routes.filter(route => route.id.includes(search.value))
-    console.log(routeSearch)
-    patch(routeWrap,
-      div({ class: 'routes' }, routeSearch.map(renderRoute)))
+  const col = await db.collection('routes').get()
+  for (const doc of col.docs) {
+    routes.push({ ...doc.data(), id: doc.id })
   }
-  patch(routeWrap,
-    div({ class: 'routes' }, routes.map(renderRoute)))
+  state.routes = routes
+  render(state)
+
+  search.oninput = _ => {
+    state.routes = routes.filter(route =>
+      (route.id + route.name).includes(search.value))
+    render(state)
+  }
+})()
+
+function render(state) {
+  patch(routewrap,
+    div({ class: 'routes' }, state.routes.map(renderRoute)))
 }
 
-// Render route function for changing the pages contents
+// Defines the HTML structure for a single bus route
 function renderRoute (route) {
   function onclick () {
     window.sessionStorage.setItem('route', route.id)
-    window.sessionStorage.setItem('stations_Id', route.data.path)
-    window.sessionStorage.setItem('endingStation', route.data.name)
+    window.sessionStorage.setItem('stations_Id', route.path)
+    window.sessionStorage.setItem('endingStation', route.name)
     window.location.href = 'route.html'
   }
-
-  return div({ class: 'option', onclick: onclick }, [
+  return div({ class: 'option', onclick }, [
     div({ class: 'option-data' }, [
       p({ class: 'option-text' }, route.id),
-      div({ class: 'option-subtext' }, route.data.name)
+      div({ class: 'option-subtext' }, route.name)
     ]),
     span({ class: 'option-icon material-icons' }, 'chevron_right')
   ])
