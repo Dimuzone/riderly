d03bb5df03057eaed2f98d3e99919c7df04d0b26
Brandon Semilla <semibran@gmail.com>
2020-12-01 14:28:11 -0800

    route page stations

--- public/scripts/route.js
+++ public/scripts/route.js
@@ -1,6 +1,6 @@
 const {
   fmtstn, normname, getstns, L: Leaflet,
-  patch, main, header, div, h1, h2, span, button
+  patch, main, header, div, h1, h2, span, p, button, input
 } = window
 
 const $main = document.querySelector('main')
@@ -8,6 +8,7 @@ const state = {
   stations: JSON.parse(window.localStorage.stations || '[]'),
   routes: JSON.parse(window.localStorage.routes || '[]'),
   search: JSON.parse(window.sessionStorage.search || '{}'),
+  filter: JSON.parse(window.sessionStorage.filter || '{}'),
   path: null
 }
 
@@ -59,21 +60,71 @@ function update (data) {
 }
 
 const RoutePage = (state) => {
-  const { route, search } = state
+  const { route, search, filter } = state
+  const stations = route.path
   return main({ class: `page -route -${route.id}` }, [
     header({ class: 'header -color -primary' }, [
       div({ class: 'header-text' }, [
         div({ class: 'title-row' }, [
           h1({ class: 'title' }, `${route.id} ${normname(route.name)}`),
           button({ class: 'back', onclick: window.history.back }, [
-            span({ class: 'icon -back material-icons' }, 'keyboard_arrow_left'),
+            span({ class: 'icon -back material-icons' },
+              'keyboard_arrow_left'),
             search.query ? 'Search' : 'Home'
           ])
         ]),
         h2({ class: 'subtitle' }, `Route ${route.id}`)
       ])
     ]),
-    div({ id: 'map', key: 'map' })
+    div({ id: 'map', key: 'map' }),
+    !stations.length
+      ? null
+      : div({ class: 'section -stops' }, [
+        div({ class: 'section-header' }, [
+          h2({ class: 'section-title' }, 'Route Overview'),
+          span({ class: 'section-subtitle' },
+            filter.query
+              ? `${filter.results.length} stops for "${filter.query}"`
+              : `${stations.length} stops`)
+        ]),
+        div({ class: 'section-content' }, [
+          div({ class: 'search' }, [
+            div({ class: 'search-bar' }, [
+              span({ class: 'icon -search material-icons' },
+                'search'),
+              input({
+                id: 'filter',
+                class: 'search-input',
+                placeholder: 'Filter stations',
+                autocomplete: 'off',
+                oninput: evt => evt
+              }),
+              filter.query
+                ? span({ class: 'icon -close material-icons', onclick: evt => evt },
+                    'close')
+                : null
+            ])
+          ]),
+          div({ class: 'stations' },
+            (filter.query ? filter.results : stations).map(stn => Station(stn, route)))
+        ])
+      ])
+  ])
+}
+
+const Station = (station, route) => {
+  const [name, subname] = fmtstn(station.name)
+  const desc = `${station.id} · ${subname}`
+  const href = `/station.html#${route.id}/${station.id}`
+  return a({ class: 'station -' + station.id, href }, [
+    div({ class: 'station-lhs' }, [
+      div({ class: 'station-meta' }, [
+        span({ class: 'station-name' }, name),
+        span({ class: 'station-desc' }, desc)
+      ])
+    ]),
+    span({ class: 'stop-rhs material-icons' },
+      'keyboard_arrow_right')
   ])
 }
 
