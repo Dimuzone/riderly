d50339d2488ca036e667fe77b757fc6bc3464843
Dimuzone <71089439+Dimuzone@users.noreply.github.com>
2020-12-04 00:35:09 +0000

    Deploying to gh-pages from  @ 88535339d58df6a5064cbe66d6311c4f70140804 🚀

--- scripts/db.js
+++ scripts/db.js
@@ -1,12 +1,28 @@
 // init firebase
 window.firebase.initializeApp({
-  apiKey: 'AIzaSyAm8r7J9OSOEU1RCMDDL3BNZo5h_jgtcXM',
-  authDomain: 'riderly-1800.firebaseapp.com',
-  databaseURL: 'https://riderly-1800.firebaseio.com',
-  projectId: 'riderly-1800',
-  storageBucket: 'riderly-1800.appspot.com',
-  messagingSenderId: '1059537142713',
-  appId: '1:1059537142713:web:eb92a5b337aa5f74d8ac68'
+  apiKey: 'AIzaSyBYV23zeovHmzX26j9H9HYyI9kBlAzZcvg',
+  authDomain: 'bus-dating.firebaseapp.com',
+  databaseURL: 'https://bus-dating.firebaseio.com',
+  projectId: 'bus-dating',
+  storageBucket: 'bus-dating.appspot.com',
+  messagingSenderId: '1012927739228',
+  appId: '1:1012927739228:web:ce582ea70ee1c0f25b7d99'
 })
+
+// apiKey: 'AIzaSyBYV23zeovHmzX26j9H9HYyI9kBlAzZcvg',
+// authDomain: 'bus-dating.firebaseapp.com',
+// databaseURL: 'https://bus-dating.firebaseio.com',
+// projectId: 'bus-dating',
+// storageBucket: 'bus-dating.appspot.com',
+// messagingSenderId: '1012927739228',
+// appId: '1:1012927739228:web:ce582ea70ee1c0f25b7d99'
+
+// apiKey: 'AIzaSyAm8r7J9OSOEU1RCMDDL3BNZo5h_jgtcXM',
+// authDomain: 'riderly-1800.firebaseapp.com',
+// databaseURL: 'https://riderly-1800.firebaseio.com',
+// projectId: 'riderly-1800',
+// storageBucket: 'riderly-1800.appspot.com',
+// messagingSenderId: '1059537142713',
+// appId: '1:1059537142713:web:eb92a5b337aa5f74d8ac68'
 
 window.db = window.firebase.firestore()
